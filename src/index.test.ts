c3466b70ac5c8b3c74795336a704818d91b9bbd8
Jörg Breitbart <jerch@rockborn.de>
2018-12-14 23:52:53 +0100

    inital

---
+++ src/index.test.ts
@@ -0,0 +1,155 @@
+import { assert } from 'chai';
+import { Utf8Decoder, Utf16Decoder } from '.';
+import { encode } from 'utf8';
+
+// convert "bytestring" (charCode 0-255) to bytes
+function fromByteString(s: string): Uint8Array {
+  const result = new Uint8Array(s.length);
+  for (let i = 0; i < s.length; ++i) {
+    result[i] = s.charCodeAt(i);
+  }
+  return result;
+}
+
+// convert codepoints (UTF32) to string
+function toByteString(data: Uint32Array, length: number): string {
+  return String.fromCodePoint.apply(null, data.subarray(0, length));
+}
+
+describe('utf8 decode', () => {
+  describe('full codepoint test', () => {
+    it('0..65535 (1/2/3 byte sequences)', () => {
+      const decoder = new Utf8Decoder();
+      const target = new Uint32Array(5);
+      for (let i = 0; i < 65536; ++i) {
+        // skip surrogate pairs
+        if (i >= 0xD800 && i <= 0xDFFF) {
+          continue;
+        }
+        const utf8_data = fromByteString(encode(String.fromCharCode(i)));
+        const length = decoder.decode(utf8_data, target);
+        assert.equal(length, 1);
+        assert.equal(toByteString(target, length), String.fromCharCode(i));
+        decoder.clear();
+      }
+    });
+    it('65536..0x10FFFF (4 byte sequences)', function(): void {
+      this.timeout(20000);
+      const decoder = new Utf8Decoder();
+      const target = new Uint32Array(5);
+      for (let i = 65536; i < 0x10FFFF; ++i) {
+        const utf8_data = fromByteString(encode(String.fromCodePoint(i)));
+        const length = decoder.decode(utf8_data, target);
+        assert.equal(length, 1);
+        assert.equal(target[0], i);
+        decoder.clear();
+      }
+    });
+  });
+  describe('stream handling', () => {
+    it('2 byte sequences - advance by 1', () => {
+      const decoder = new Utf8Decoder();
+      const target = new Uint32Array(5);
+      const utf8_data = fromByteString('\xc3\x84\xc3\x96\xc3\x9c\xc3\x9f\xc3\xb6\xc3\xa4\xc3\xbc');
+      let decoded = '';
+      for (let i = 0; i < utf8_data.length; ++i) {
+        const written = decoder.decode(utf8_data.slice(i, i + 1), target);
+        decoded += toByteString(target, written);
+      }
+      assert(decoded, 'ÄÖÜßöäü');
+    });
+    it('2/3 byte sequences - advance by 1', () => {
+      const decoder = new Utf8Decoder();
+      const target = new Uint32Array(5);
+      const utf8_data = fromByteString('\xc3\x84\xe2\x82\xac\xc3\x96\xe2\x82\xac\xc3\x9c\xe2\x82\xac\xc3\x9f\xe2\x82\xac\xc3\xb6\xe2\x82\xac\xc3\xa4\xe2\x82\xac\xc3\xbc');
+      let decoded = '';
+      for (let i = 0; i < utf8_data.length; ++i) {
+        const written = decoder.decode(utf8_data.slice(i, i + 1), target);//𝄞
+        decoded += toByteString(target, written);
+      }
+      assert(decoded, 'Ä€Ö€Ü€ß€ö€ä€ü');
+    });
+    it('2/3/4 byte sequences - advance by 1', () => {
+      const decoder = new Utf8Decoder();
+      const target = new Uint32Array(5);
+      const utf8_data = fromByteString('\xc3\x84\xe2\x82\xac\xf0\x9d\x84\x9e\xc3\x96\xf0\x9d\x84\x9e\xe2\x82\xac\xc3\x9c\xf0\x9d\x84\x9e\xe2\x82\xac');
+      let decoded = '';
+      for (let i = 0; i < utf8_data.length; ++i) {
+        const written = decoder.decode(utf8_data.slice(i, i + 1), target);
+        decoded += toByteString(target, written);
+      }
+      assert(decoded, 'Ä€𝄞Ö𝄞€Ü𝄞€');
+    });
+    it('2/3/4 byte sequences - advance by 2', () => {
+      const decoder = new Utf8Decoder();
+      const target = new Uint32Array(5);
+      const utf8_data = fromByteString('\xc3\x84\xe2\x82\xac\xf0\x9d\x84\x9e\xc3\x96\xf0\x9d\x84\x9e\xe2\x82\xac\xc3\x9c\xf0\x9d\x84\x9e\xe2\x82\xac');
+      let decoded = '';
+      for (let i = 0; i < utf8_data.length; i += 2) {
+        const written = decoder.decode(utf8_data.slice(i, i + 2), target);
+        decoded += toByteString(target, written);
+      }
+      assert(decoded, 'Ä€𝄞Ö𝄞€Ü𝄞€');
+    });
+    it('2/3/4 byte sequences - advance by 3', () => {
+      const decoder = new Utf8Decoder();
+      const target = new Uint32Array(5);
+      const utf8_data = fromByteString('\xc3\x84\xe2\x82\xac\xf0\x9d\x84\x9e\xc3\x96\xf0\x9d\x84\x9e\xe2\x82\xac\xc3\x9c\xf0\x9d\x84\x9e\xe2\x82\xac');
+      let decoded = '';
+      for (let i = 0; i < utf8_data.length; i += 3) {
+        const written = decoder.decode(utf8_data.slice(i, i + 3), target);
+        decoded += toByteString(target, written);
+      }
+      assert(decoded, 'Ä€𝄞Ö𝄞€Ü𝄞€');
+    });
+  });
+});
+
+describe('utf16 decode', () => {
+  describe('full codepoint test', () => {
+    it('0..65535', () => {
+      const decoder = new Utf16Decoder();
+      const target = new Uint32Array(5);
+      const utf16_data = new Uint16Array(1);
+      for (let i = 0; i < 65536; ++i) {
+        // skip surrogate pairs
+        if (i >= 0xD800 && i <= 0xDFFF) {
+          continue;
+        }
+        utf16_data[0] = i;
+        const length = decoder.decode(utf16_data, target);
+        assert.equal(length, 1);
+        assert.equal(toByteString(target, length), String.fromCharCode(i));
+        decoder.clear();
+      }
+    });
+    it('65536..0x10FFFF (surrogates)', function(): void {
+      this.timeout(20000);
+      const decoder = new Utf16Decoder();
+      const target = new Uint32Array(5);
+      const utf16_data = new Uint16Array(2);
+      for (let i = 65536; i < 0x10FFFF; ++i) {
+        const s = String.fromCodePoint(i);
+        utf16_data[0] = s.charCodeAt(0);
+        utf16_data[1] = s.charCodeAt(1);
+        const length = decoder.decode(utf16_data, target);
+        assert.equal(length, 1);
+        assert.equal(target[0], i);
+        decoder.clear();
+      }
+    });
+  });
+  describe('stream handling', () => {
+    it('surrogates mixed advance by 1', () => {
+      const decoder = new Utf16Decoder();
+      const target = new Uint32Array(5);
+      const utf16_data = new Uint16Array(fromByteString('\xc4\x00\xac 4\xd8\x1e\xdd\xd6\x004\xd8\x1e\xdd\xac \xdc\x004\xd8\x1e\xdd\xac ').buffer);
+      let decoded = '';
+      for (let i = 0; i < utf16_data.length; ++i) {
+        const written = decoder.decode(utf16_data.slice(i, i + 1), target);
+        decoded += toByteString(target, written);
+      }
+      assert(decoded, 'Ä€𝄞Ö𝄞€Ü𝄞€');
+    });
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,327 @@
+/**
+ * Text Trancoders based on typed arrays.
+ * 
+ * Note on endianess: Since the typed arrays are meant for internal representation only,
+ * the endianess follows the system default.
+ * 
+ * TODO: customizable error handling:
+ *    - ignore:   faulty sequences are silently swallowed (currently hardcoded)
+ *    - replace:  replace faulty sequences with '\uFFFD' (recommended by unicode consortium)
+ *    - strict:   raise Error
+ */
+
+
+/**
+ * Utf8Decoder - decodes UTF8 byte sequences into UTF32 codepoints.
+ */
+export class Utf8Decoder {
+  public interim: Uint8Array = new Uint8Array(3);
+
+  /**
+   * Clears interim bytes and resets decoder to clean state.
+   */
+  public clear(): void {
+    this.interim.fill(0);
+  }
+
+  /**
+   * Decodes UTF8 byte sequences in `input` to UTF32 codepoints in `target`.
+   * The methods assumes stream input and will store partly transmitted bytes
+   * and decode them with the next data chunk.
+   * Note: The method does no bound checks for target, therefore make sure
+   * the provided data chunk does not exceed the size of `target`.
+   */
+  decode(input: Uint8Array, target: Uint32Array): number {
+    const length = input.length;
+
+    if (!length) {
+      return 0;
+    }
+
+    let size = 0;
+    let byte1, byte2, byte3, byte4;
+    let codepoint = 0;
+    let startPos = 0;
+
+    // handle leftover bytes
+    if (this.interim[0]) {
+      let discardInterim = false;
+      let cp = this.interim[0];
+      cp &= ((((cp & 0xE0) == 0xC0)) ? 0x1F : (((cp & 0xF0) == 0xE0)) ? 0x0F : 0x07);
+      let pos = 0;
+      let tmp;
+      while ((tmp = this.interim[++pos] & 0x3F) && pos < 4) {
+        cp <<= 6;
+        cp |= tmp;
+      }
+      // missing bytes - read ahead from input
+      const type = (((this.interim[0] & 0xE0) == 0xC0)) ? 2 : (((this.interim[0] & 0xF0) == 0xE0)) ? 3 : 4;
+      let missing = type - pos;
+      while (startPos < missing) {
+        if (startPos >= length) {
+          return 0;
+        }
+        tmp = input[startPos++];
+        if ((tmp & 0xC0) !== 0x80) {
+          // wrong continuation, discard interim bytes completely
+          startPos--;
+          discardInterim = true;
+          break;
+        } else {
+          // need to save so we can continue short inputs in next call
+          this.interim[pos++] = tmp;
+          cp <<= 6;
+          cp |= tmp & 0x3F;
+        }
+      }
+      if (!discardInterim) {
+        // final test is type dependent
+        if (type === 2) {
+          if (cp < 0x80) {
+            // wrong starter byte
+            startPos--;
+          } else {
+            target[size++] = cp;
+          }
+        } else if (type === 3) {
+          if (cp < 0x0800 || (cp >= 0xD800 && cp <= 0xDFFF)) {
+            // illegal codepoint
+          } else {
+            target[size++] = cp;
+          }
+        } else {
+          if (codepoint < 0x010000 || codepoint > 0x10FFFF) {
+            // illegal codepoint
+          } else {
+            target[size++] = cp;
+          }
+        }
+      }
+      this.interim.fill(0);
+    }
+
+    // loop through input
+    for (let i = startPos; i < length;) {
+        byte1 = input[i++];
+
+        // 1 byte
+        if (byte1 < 0x80) {
+          target[size++] = byte1;
+        
+        // 2 bytes
+        } else if ((byte1 & 0xE0) == 0xC0) {
+          if (i >= length) {
+            this.interim[0] = byte1;
+            return size;
+          }
+          byte2 = input[i++];
+          if ((byte2 & 0xC0) !== 0x80) {
+            // wrong continuation
+            i--;
+            continue;
+          }
+          codepoint = (byte1 & 0x1F) << 6 | (byte2 & 0x3F);
+          if (codepoint < 0x80) {
+            // wrong starter byte
+            i--;
+            continue;
+          }
+          target[size++] = codepoint;
+        
+        // 3 bytes
+        } else if ((byte1 & 0xF0) == 0xE0) {
+          if (i >= length) {
+            this.interim[0] = byte1;
+            return size;
+          }
+          byte2 = input[i++];
+          if ((byte2 & 0xC0) !== 0x80) {
+            // wrong continuation
+            i--;
+            continue;
+          }
+          if (i >= length) {
+            this.interim[0] = byte1;
+            this.interim[1] = byte2;
+            return size;
+          }
+          byte3 = input[i++];
+          if ((byte3 & 0xC0) !== 0x80) {
+            // wrong continuation
+            i--;
+            continue;
+          }
+          codepoint = (byte1 & 0x0F) << 12 | (byte2 & 0x3F) << 6 | (byte3 & 0x3F);
+          if (codepoint < 0x0800 || (codepoint >= 0xD800 && codepoint <= 0xDFFF)) {
+            // illegal codepoint, no i-- here
+            continue;
+          }
+          target[size++] = codepoint;
+
+        // 4 bytes
+        } else if ((byte1 & 0xF8) == 0xF0) {
+          if (i >= length) {
+            this.interim[0] = byte1;
+            return size;
+          }
+          byte2 = input[i++];
+          if ((byte2 & 0xC0) !== 0x80) {
+            // wrong continuation
+            i--;
+            continue;
+          }
+          if (i >= length) {
+            this.interim[0] = byte1;
+            this.interim[1] = byte2;
+            return size;
+          }
+          byte3 = input[i++];
+          if ((byte3 & 0xC0) !== 0x80) {
+            // wrong continuation
+            i--;
+            continue;
+          }
+          if (i >= length) {
+            this.interim[0] = byte1;
+            this.interim[1] = byte2;
+            this.interim[2] = byte3;
+            return size;
+          }
+          byte4 = input[i++];
+          if ((byte4 & 0xC0) !== 0x80) {
+            // wrong continuation
+            i--;
+            continue;
+          }
+          codepoint = (byte1 & 0x07) << 18 | (byte2 & 0x3F) << 12 | (byte3 & 0x3F) << 6 | (byte4 & 0x3F);
+          if (codepoint < 0x010000 || codepoint > 0x10FFFF) {
+            // illegal codepoint, no i-- here
+            continue;
+          }
+          target[size++] = codepoint;
+        } else {
+          // illegal byte, just skip
+        }
+    }
+    return size;
+  }
+}
+
+/**
+ * Utf16Decoder - decodes UTF16 sequences into UTF32 codepoints.
+ * To keep the decoder in line with JS strings it handles single surrogates as UCS2.
+ */
+export class Utf16Decoder {
+  public interim: number = 0;
+
+  /**
+   * Clears interim and resets decoder to clean state.
+   */
+  public clear(): void {
+    this.interim = 0;
+  }
+
+  /**
+   * Decodes UTF16 sequences in `input` to UTF32 codepoints in `target`.
+   * The methods assumes stream input and will store partly transmitted
+   * surrogate pairs and decode them with the next data chunk.
+   * Note: The method does no bound checks for target, therefore make sure
+   * the provided input data does not exceed the size of `target`.
+   */
+  decode(input: Uint16Array, target: Uint32Array): number {
+    const length = input.length;
+
+    if (!length) {
+      return 0;
+    }
+
+    let size = 0;
+    let startPos = 0;
+
+    if (this.interim) {
+      const second = input[startPos++];
+      if (0xDC00 <= second && second <= 0xDFFF) {
+        target[size++] = (this.interim - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
+      } else {
+        // illegal codepoint (USC2 handling)
+        target[size++] = this.interim;
+        target[size++] = second;
+      }
+      this.interim = 0;
+    }
+
+    for (let i = startPos; i < length; ++i) {
+      let code = input[i];
+      // surrogate pair first
+      if (0xD800 <= code && code <= 0xDBFF) {
+        if (++i >= length) {
+          this.interim = code;
+          return size;
+        }
+        const second = input[i];
+        if (0xDC00 <= second && second <= 0xDFFF) {
+          target[size++] = (code - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
+        } else {
+          // illegal codepoint (USC2 handling)
+          target[size++] = code;
+          target[size++] = second;
+        }
+        continue;
+      }
+      target[size++] = code;
+    }
+    return size;
+  }
+
+  /**
+   * Decode JS string to UTF32 codepoints.
+   * The methods assumes stream input and will store partly transmitted
+   * surrogate pairs and decode them with the next data chunk.
+   * Note: The method does no bound checks for target, therefore make sure
+   * the provided input data does not exceed the size of `target`.
+   */
+  decodeString(input: string, target: Uint32Array): number {
+    const length = input.length;
+
+    if (!length) {
+      return 0;
+    }
+
+    let size = 0;
+    let startPos = 0;
+
+    if (this.interim) {
+      const second = input.charCodeAt(startPos++);
+      if (0xDC00 <= second && second <= 0xDFFF) {
+        target[size++] = (this.interim - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
+      } else {
+        // illegal codepoint (USC2 handling)
+        target[size++] = this.interim;
+        target[size++] = second;
+      }
+      this.interim = 0;
+    }
+
+    for (let i = startPos; i < length; ++i) {
+      let code = input.charCodeAt(i);
+      // surrogate pair first
+      if (0xD800 <= code && code <= 0xDBFF) {
+        if (++i >= length) {
+          this.interim = code;
+          return size;
+        }
+        const second = input.charCodeAt(i);
+        if (0xDC00 <= second && second <= 0xDFFF) {
+          target[size++] = (code - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
+        } else {
+          // illegal codepoint (USC2 handling)
+          target[size++] = code;
+          target[size++] = second;
+        }
+        continue;
+      }
+      target[size++] = code;
+    }
+    return size;
+  }
+}
